46fade9d18ad885e0bd689a7f8c45e3989763e21
steinbrennergit <steinbrennergit@gmail.com>
2018-08-09 13:59:51 -0700

    changed connection settings for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql');
 
 const con = mysql.createConnection({
-    host: "localhost",
+    host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
     // Your port; if not 3306
     port: 3306,
 
     // Your username
-    user: "root",
+    user: "i46buqu6ft76tq5o",
 
     // Your password
-    password: "root",
-    database: "burgers_db"
+    password: "yguyh99wqvbb6orp",
+    database: "h1ghpl9vtprg53o3"
 });
 
 module.exports = con;
