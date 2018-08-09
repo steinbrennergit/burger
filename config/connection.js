c05f28f53484680cc09571f735ea930760384672
steinbrennergit <steinbrennergit@gmail.com>
2018-08-09 14:04:24 -0700

    changing connection settings for mysql

--- config/connection.js
+++ config/connection.js
@@ -1,17 +1,22 @@
 const mysql = require('mysql');
 
-const con = mysql.createConnection({
-    host: "l6slz5o3eduzatkw.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+var con;
+if (process.env.JAWSDB_URL) {
+    con = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    con = mysql.createConnection({
+        host: "localhost",
 
-    // Your port; if not 3306
-    port: 3306,
+        // Your port; if not 3306
+        port: 3306,
 
-    // Your username
-    user: "i46buqu6ft76tq5o",
+        // Your username
+        user: "root",
 
-    // Your password
-    password: "yguyh99wqvbb6orp",
-    database: "h1ghpl9vtprg53o3"
-});
+        // Your password
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 module.exports = con;
